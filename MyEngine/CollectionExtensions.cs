0a08540cb17ea645ffe287792b0ea72a23147e69
Serhii <serikgorbanev@gmail.com>
2025-01-09 23:24:35 +0100

    Renamed Engine project to MyEngine

---
+++ MyEngine/CollectionExtensions.cs
@@ -0,0 +1,10 @@
+namespace MyEngine;
+
+public static class CollectionExtensions
+{
+    public static void Enqueue<T>(this Queue<T> queue, IEnumerable<T> enqueued)
+    {
+        foreach (T each in enqueued)
+            queue.Enqueue(each);
+    }
+}
