b701087fc05fb89104cc7acc8c66f4fa389005a4
Serhii <serikgorbanev@gmail.com>
2025-01-11 10:46:39 +0100

    Added LengthSquared to VectorMath

--- MyEngine/VectorMath.cs
+++ MyEngine/VectorMath.cs
@@ -4,6 +4,9 @@ namespace MyEngine;
 
 public static class VectorMath
 {
+    public static float LengthSquared(this Vector2f vector)
+        => vector.SquaredDistanceTo(new Vector2f(0, 0));
+    
     public static float SquaredDistanceTo(this Vector2f from, Vector2f to)
     {
         Vector2f diff = from - to;
