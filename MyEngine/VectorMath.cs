2e59eb54859184b7a13f2bd57fea22e683bf305c
Serhii <serikgorbanev@gmail.com>
2025-01-14 06:00:25 +0100

    Added Length and Lerp to VectorMath

--- MyEngine/VectorMath.cs
+++ MyEngine/VectorMath.cs
@@ -7,9 +7,15 @@ public static class VectorMath
     public static float LengthSquared(this Vector2f vector)
         => vector.SquaredDistanceTo(new Vector2f(0, 0));
     
+    public static float Length(this Vector2f vector)
+        => float.Sqrt(vector.LengthSquared());
+    
     public static float SquaredDistanceTo(this Vector2f from, Vector2f to)
     {
         Vector2f diff = from - to;
         return (diff.X * diff.X) + (diff.Y * diff.Y);
     }
+
+    public static Vector2f Lerp(this Vector2f from, Vector2f to, float interpolation)
+        => from + ((to - from) * interpolation);
 }
