1ff460934cc888b11bf0e9b4b314cdfcd35738c0
Serhii <serikgorbanev@gmail.com>
2025-01-14 17:52:24 +0100

    Added Player, AIController, PlayerController

---
+++ Agario 2/AIController.cs
@@ -0,0 +1,51 @@
+using MyEngine;
+using MyEngine.Nodes;
+using SFML.System;
+
+namespace Agario_2;
+
+public class AiController : Node
+{
+    private Player _player;
+    private Vector2f _currentWayPoint;
+
+    private const float MaxDistanceToWayPoint = 200;
+    private const float DistanceSquaredForNewWayPoint = 100;
+    
+    private AiController(Player player)
+    {
+        _player = player;
+        SetNewWayPoint();
+    }
+
+    public static AiController CreateAiController(Player player)
+        => new(player);
+
+    private void SetNewWayPoint()
+    {
+        float x = Random.Shared.NextSingle() * MaxDistanceToWayPoint * 2 - MaxDistanceToWayPoint;
+        float y = Random.Shared.NextSingle() * MaxDistanceToWayPoint * 2 - MaxDistanceToWayPoint;
+        
+        _currentWayPoint = _player.Position + new Vector2f(x, y);
+    }
+    
+    protected override void Update(Node root)
+    {
+        UpdateDelta();
+        
+        if (TooCloseToWayPoint())
+            SetNewWayPoint();
+    }
+
+    private bool TooCloseToWayPoint()
+        => _player.Position.SquaredDistanceTo(_currentWayPoint) < DistanceSquaredForNewWayPoint;
+    
+    private void UpdateDelta()
+    {
+        Vector2f delta = _currentWayPoint - _player.Position;
+        delta /= delta.Length();
+        delta *= _player.MaxSpeed;
+        
+        _player.WishedDelta = delta;
+    }
+}
