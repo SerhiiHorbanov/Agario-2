4ea70aaf1fcd29a6d6f9e5932d9e822e8851e420
Serhii <serikgorbanev@gmail.com>
2025-01-18 10:34:42 +0100

    Added RandomPositionInside to VectorMath

--- MyEngine/VectorMath.cs
+++ MyEngine/VectorMath.cs
@@ -1,3 +1,4 @@
+using SFML.Graphics;
 using SFML.System;
 
 namespace MyEngine;
@@ -18,4 +19,12 @@ public static class VectorMath
 
     public static Vector2f Lerp(this Vector2f from, Vector2f to, float interpolation)
         => from + ((to - from) * interpolation);
+
+    public static Vector2f RandomPositionInside(this FloatRect bounds)
+    {
+        float x = Random.Shared.NextSingle() * bounds.Width + bounds.Left;
+        float y = Random.Shared.NextSingle() * bounds.Height + bounds.Top;
+        
+        return new Vector2f(x, y);
+    }
 }
