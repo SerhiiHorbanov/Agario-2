3db15328b3c0ea5126292494d536fb60eb8b49a8
Serhii <serikgorbanev@gmail.com>
2025-01-27 15:03:52 +0100

    Reverted "Optimized camera view changing". Did some cleanup

--- MyEngine/Nodes/Camera.cs
+++ MyEngine/Nodes/Camera.cs
@@ -7,37 +7,21 @@ public class Camera : Node
 {
     private View _view;
     private RenderTarget _target;
-
-
-    private bool changedView;
     
     public Vector2f Position
     {
         get => _view.Center;
-        set
-        {
-            _view.Center = value;
-            changedView = true;
-        }
+        set => _view.Center = value;
     }
 
     public Vector2f Size
     {
         get => _view.Size;
-        set
-        {
-            _view.Size = value;
-            changedView = true;
-        }
+        set => _view.Size = value;
     }
 
-    public Vector2f RenderTargetSize
-        => (Vector2f)_target.Size;
-    
     private Camera()
-    {
-        _view = new();
-    }
+        => _view = new();
 
     public static Camera CreateCamera(RenderTarget target)
     {
@@ -50,18 +34,15 @@ public class Camera : Node
     
     public void Render(Node rootNode)
     {
-        UpdateView();
-        
         Queue<Drawable> drawables = rootNode.GetRenderQueue(this);
 
+        ApplyView();
         _target.Clear(Color.Black);
+        
         foreach (Drawable drawable in drawables)
             drawable.Draw(_target, RenderStates.Default);
     }
-
-    private void UpdateView()
-    {
-        if (changedView)
-            _target.SetView(_view);
-    }
+    
+    private void ApplyView()
+        => _target.SetView(_view);
 }
