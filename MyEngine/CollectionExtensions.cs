84d73355918f4b8356763385fc49ddf0d4e2123e
Serhii <serikgorbanev@gmail.com>
2025-01-27 14:53:48 +0100

    Added SwapRemove and Swap to CollectionExtensions

--- MyEngine/CollectionExtensions.cs
+++ MyEngine/CollectionExtensions.cs
@@ -10,4 +10,13 @@ public static class CollectionExtensions
 
     public static T GetRandomElement<T>(this List<T> list)
         => list[Random.Shared.Next(list.Count)];
+
+    public static void SwapRemoveAt<T>(this List<T> list, int index)
+    {
+        list.Swap(index, list.Count - 1);
+        list.RemoveAt(list.Count - 1);
+    }
+
+    private static void Swap<T>(this List<T> list, int first, int second)
+        => (list[first], list[second]) = (list[second], list[first]);
 }
