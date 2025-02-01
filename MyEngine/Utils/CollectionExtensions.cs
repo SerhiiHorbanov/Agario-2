1971dedc020b11455facb92d85e0a3843d0ca0bc
Serhii <serikgorbanev@gmail.com>
2025-02-01 09:09:23 +0100

    Moved many classes in MyEngine

---
+++ MyEngine/Utils/CollectionExtensions.cs
@@ -0,0 +1,34 @@
+namespace MyEngine;
+
+public static class CollectionExtensions
+{
+    public static void Enqueue<T>(this Queue<T> queue, IEnumerable<T> enqueued)
+    {
+        foreach (T each in enqueued)
+            queue.Enqueue(each);
+    }
+
+    public static T GetRandomElement<T>(this List<T> list)
+        => list[Random.Shared.Next(list.Count)];
+
+    public static void SwapRemove<T>(this List<T> list, T item) where T : class
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i] == item)
+            {
+                list.SwapRemoveAt(i);
+                return;
+            }
+        }
+    }
+    
+    public static void SwapRemoveAt<T>(this List<T> list, int index)
+    {
+        list.Swap(index, list.Count - 1);
+        list.RemoveAt(list.Count - 1);
+    }
+
+    private static void Swap<T>(this List<T> list, int first, int second)
+        => (list[first], list[second]) = (list[second], list[first]);
+}
