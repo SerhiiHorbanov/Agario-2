21173c575c889c9f32b83201f3c6182f2e7f29e3
Serhii <serikgorbanev@gmail.com>
2025-02-07 22:59:01 +0100

    Added Dash sound file. Added SoundFilesConfigs

---
+++ Agario 2/Source/Configs/SoundFilesConfigs.cs
@@ -0,0 +1,11 @@
+namespace Agario_2.Configs;
+
+public class SoundFilesConfigs
+{
+    public SoundFilesConfigs(string dashFile)
+    {
+        DashFile = dashFile;
+    }
+
+    public string DashFile = "Sounds/Dash.mp3";
+}
